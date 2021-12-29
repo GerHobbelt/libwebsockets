c1c2d558cbd6118a26ec54119f3236d1bec3a32e
Andy Green <andy@warmcat.com>
2021-12-29 07:11:15 +0000

    ldo-png-registry
    Provides a temporary way to register PNG blobs bound to "filenames", so
    they can be found if referenced by html.

--- lib/core/private-lib-core.h
+++ lib/core/private-lib-core.h
@@ -451,6 +451,7 @@ struct lws_context {
 
 #if defined(LWS_WITH_DLO)
 	lws_dll2_owner_t		fonts;
+	lws_dll2_owner_t		pngs;
 #endif
 
 #if defined(LWS_WITH_NETWORK)
@@ -541,6 +542,7 @@ struct lws_context {
 #if defined(LWS_WITH_DRIVERS)
 	lws_netdevs_t			netdevs;
 #endif
+
 #if defined(LWS_WITH_SYS_ASYNC_DNS)
 	lws_async_dns_t			async_dns;
 #endif
